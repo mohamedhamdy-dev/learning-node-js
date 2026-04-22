5d359382cf6071bed5877fec107c593d5c69a30e
Mohamed Hamdy <149756565+mohamedhamdy-dev@users.noreply.github.com>
2026-04-22 11:42:26 +0200

    make some routes and know about middlewares and serving some html

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-import http from "http";
-import { requestHandler } from "./routes.js";
+import express from "express";
+import bodyParser from "body-parser";
+import adminRoutes from "./routes/adminRoutes.js";
+import shopRoutes from "./routes/shopRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const server = http.createServer(requestHandler);
+////////////////////
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+////////////////////
 
-server.listen(3001);
+const app = express();
+app.use(bodyParser.urlencoded());
+
+// Routes
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+// 404 Fallback
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
+});
+
+// Server Listen to requests on port 3001
+app.listen(3001);
---
+++ routes/adminRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+////////////////////
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+////////////////////
+
+const router = express.Router();
+
+router.get("/add-product", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+});
+
+router.post("/add-product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+export default router;
